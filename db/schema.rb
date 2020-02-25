3231df45bf99795ba63b75015c44208cecbb7ab0
AntonAlmosov <almosoww@gmail.com>
2020-02-25 17:48:50 +0300

    Started to make layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_194533) do
+ActiveRecord::Schema.define(version: 2020_02_03_212228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "magasines", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "external_link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +39,20 @@ ActiveRecord::Schema.define(version: 2020_02_03_194533) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
