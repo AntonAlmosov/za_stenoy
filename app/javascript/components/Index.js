136535ad5cb0bf77a7ae50f7f702fbb6fe23b4df
AntonAlmosov <almosoww@gmail.com>
2020-03-01 02:12:00 +0300

    Came upon strange bug

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Header from "./templates/Header";
+
+export default function Index(props) {
+  return <div className="app-wrapper">{/* <Header /> */}</div>;
+}
---
+++ app/javascript/components/templates/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header-wrapper">
+      <div className="header-wrapper">
+        <a href="/">Поиск</a>
+        <a href="/" className="logo"></a>
+        <a href="/">Меню</a>
+      </div>
+    </div>
+  );
+}
