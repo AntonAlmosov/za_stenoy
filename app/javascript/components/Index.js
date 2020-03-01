827229e805b03314a55aeb03c8a0e95349b90129
AntonAlmosov <almosoww@gmail.com>
2020-03-01 14:10:42 +0300

    Splited header layout into its own components and started to declutter application.scss

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,6 +1,10 @@
 import React from "react";
-import Header from "./templates/Header";
+import Header from "../components/templates/Header";
 
-export default function Index(props) {
-  return <div className="app-wrapper">{/* <Header /> */}</div>;
+export default function Index() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
--- app/javascript/components/templates/Header.jsx
+++ app/javascript/components/templates/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import logo from "images/logo-large.svg";
 
 export default function Header() {
   return (
     <div className="header-wrapper">
-      <div className="header-wrapper">
+      <div className="header">
         <a href="/">Поиск</a>
-        <a href="/" className="logo"></a>
+        <a href="/" className="logo">
+          <img src={logo} />
+        </a>
         <a href="/">Меню</a>
       </div>
     </div>
