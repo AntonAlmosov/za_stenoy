19edcdecf7a0dd44044ec8c36653d98ccfbb978f
AntonAlmosov <almosoww@gmail.com>
2020-03-03 14:54:16 +0300

    Added index starter layout

---
+++ app/javascript/components/organisms/Starters/IndexStarter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import flagsImage from "images/flags-black.svg";
+
+export default function IndexStarter() {
+  return (
+    <div className="starter-wrapper index-starter">
+      <img src={flagsImage} />
+      <p>
+        Сайт литературного издательства
+        <a href="/">«Флаги»</a>
+        <br />
+        2020
+      </p>
+    </div>
+  );
+}
--- app/javascript/components/templates/IndexTemplate.js
+++ app/javascript/components/templates/IndexTemplate.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderOrganism from "../organisms/HeaderOrganism";
+import IndexStarter from "../organisms/Starters/IndexStarter";
 
 export default function IndexTemplate() {
   return (
-    <div>
+    <>
       <HeaderOrganism logo={false} inverse={false} />
-    </div>
+      <IndexStarter />
+    </>
   );
 }
