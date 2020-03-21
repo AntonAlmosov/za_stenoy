7ab088f0aff6375070a13ba0357399b86225e37f
AntonAlmosov <almosoww@gmail.com>
2020-03-22 00:41:54 +0300

    asdas

--- app/javascript/components/templates/admin/MagasineTempalte.js
+++ app/javascript/components/templates/admin/MagasineTempalte.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HeaderAdminOrganism from "../../organisms/HeaderAdminOrganism.jsx";
-import AdminStarter from "../../organisms/starters/AdminStarter.jsx";
+import AdminStarter from "../../organisms/Starters/AdminStarter.jsx";
 import MagasineActions from "../../organisms/actions/MagasineActions.jsx";
 import MagasineTable from "../../organisms/tables/MagasineTable.jsx";
 
