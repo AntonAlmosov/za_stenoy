445daf66079390174855d4b29bbe55b9e403581e
AntonAlmosov <almosoww@gmail.com>
2020-05-25 18:12:07 +0300

    as

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :registerable
   devise :database_authenticatable, :rememberable, :validatable
 end
