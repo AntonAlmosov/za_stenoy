55083b8cd5dcaa3810145f293541d75484342f9c
AntonAlmosov <almosoww@gmail.com>
2020-06-24 21:06:55 +0300

    Editor fix

--- app/javascript/components/organisms/Content/EditorData.jsx
+++ app/javascript/components/organisms/Content/EditorData.jsx
@@ -3,49 +3,50 @@ import ReactHtmlParser from "react-html-parser";
 import defineAlligning from "../../misc/defineAlign";
 
 export default ({ text }) => {
-  return (
-    <div className="editor-parsed">
-      {text.map((line, i) => {
-        if (line.type === "header") {
-          switch (line.data.level) {
-            case 1:
-              return <h1 key={i}>{ReactHtmlParser(line.data.text)}</h1>;
-            case 2:
-              return <h2 key={i}>{ReactHtmlParser(line.data.text)}</h2>;
-            case 3:
-              return <h3 key={i}>{ReactHtmlParser(line.data.text)}</h3>;
+  if (text)
+    return (
+      <div className="editor-parsed">
+        {text.map((line, i) => {
+          if (line.type === "header") {
+            switch (line.data.level) {
+              case 1:
+                return <h1 key={i}>{ReactHtmlParser(line.data.text)}</h1>;
+              case 2:
+                return <h2 key={i}>{ReactHtmlParser(line.data.text)}</h2>;
+              case 3:
+                return <h3 key={i}>{ReactHtmlParser(line.data.text)}</h3>;
+            }
           }
-        }
-        if (line.type === "list") {
-          switch (line.data.style) {
-            case "unordered":
-              return (
-                <ul key={i}>
-                  {line.data.items.map((item, y) => {
-                    return <li key={i + y}>{ReactHtmlParser(item)}</li>;
-                  })}
-                </ul>
-              );
-            case "ordered":
-              return (
-                <ol key={i}>
-                  {line.data.items.map((item, y) => {
-                    return <li key={i + y}>{ReactHtmlParser(item)}</li>;
-                  })}
-                </ol>
-              );
+          if (line.type === "list") {
+            switch (line.data.style) {
+              case "unordered":
+                return (
+                  <ul key={i}>
+                    {line.data.items.map((item, y) => {
+                      return <li key={i + y}>{ReactHtmlParser(item)}</li>;
+                    })}
+                  </ul>
+                );
+              case "ordered":
+                return (
+                  <ol key={i}>
+                    {line.data.items.map((item, y) => {
+                      return <li key={i + y}>{ReactHtmlParser(item)}</li>;
+                    })}
+                  </ol>
+                );
+            }
           }
-        }
-        if (line.type === "paragraph") {
-          const className = defineAlligning(line.data);
-          return (
-            <p key={i} className={className}>
-              {ReactHtmlParser(line.data.text)}
-            </p>
-          );
-        }
-        if (line.type === "delimiter") return <hr key={i} />;
-      })}
-    </div>
-  );
+          if (line.type === "paragraph") {
+            const className = defineAlligning(line.data);
+            return (
+              <p key={i} className={className}>
+                {ReactHtmlParser(line.data.text)}
+              </p>
+            );
+          }
+          if (line.type === "delimiter") return <hr key={i} />;
+        })}
+      </div>
+    );
 };
